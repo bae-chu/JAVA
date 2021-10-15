30b1b9f56e6da8de38fec550b7c5d3fc875eb925
bae-chu <91940299+bae-chu@users.noreply.github.com>
2021-10-15 12:37:02 +0900

    Class

---
+++ Chapter5/FunctionTest.java
@@ -0,0 +1,41 @@
+package classpart;
+
+public class FunctionTest {
+	public static void main(String[] args) {
+
+		int num1 = 10;
+		int num2 = 20;
+
+		int sum1 = add(num1, num2);
+		int sum2 = sub(num1, num2);
+		int sum3 = gob(num1, num2);
+		double
+		sum4 = div(num1, num2);
+		System.out.println(num1 + "+" + num2 + "=" + sum1 + "입니다.");
+		System.out.println(num1 + "-" + num2 + "=" + sum2 + "입니다.");
+		System.out.println(num1 + "×" + num2 + "=" + sum3 + "입니다.");
+		System.out.println(num1 + "÷" + num2 + "=" + sum4 + "입니다.");
+		}
+
+	public static int add(int n1, int n2) {
+		int result = n1+n2;
+		return result;
+		//return n1+n2 위에 두줄처럼 안적고 이렇게 적어도 됨
+	}
+	
+	public static int sub(int n1, int n2) {
+		int result = n1-n2;
+		return result;
+		
+	}
+	public static int gob(int n1, int n2) {
+		int result = n1*n2;
+		return result;
+		
+	}
+	public static double div(int n1, int n2) {
+		double result = (double)n1/n2;
+		return result;
+		
+	}
+}
