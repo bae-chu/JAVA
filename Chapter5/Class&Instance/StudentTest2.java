313a6d3451c08aeaf446705e1da82d60a9bc4f56
bae-chu <91940299+bae-chu@users.noreply.github.com>
2021-10-19 13:52:04 +0900

    Update and rename Chapter5/StudentTest2.java to Chapter5/Class&Instance/StudentTest2.java

--- Chapter5/Class&Instance/StudentTest2.java
+++ Chapter5/Class&Instance/StudentTest2.java
@@ -4,10 +4,10 @@ public class StudentTest2 {
 	public static void main(String[] args) {
 
 		Student student1 = new Student();
-		student1.studentName = "안연수";
+		student1.studentName = "Ahnyoen";
 
 		Student student2 = new Student();
-		student2.studentName = "안승연";
+		student2.studentName = "Ahnseong";
 
 		System.out.println(student1);
 		System.out.println(student2);
