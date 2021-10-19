5bf33436cb0f5322e58ccbcafc414a26ecff5384
bae-chu <91940299+bae-chu@users.noreply.github.com>
2021-10-19 15:11:40 +0900

    hiding

---
+++ Chapter5/hiding/Student.java
@@ -0,0 +1,35 @@
+package hiding;
+
+public class Student {
+	
+	int studentId;
+	private String studentName;
+	int grade;
+	String address;
+	
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
