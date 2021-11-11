0aff1e97bf502f542bd9cac59acf7cb4bdbd37f4
bae-chu <91940299+bae-chu@users.noreply.github.com>
2021-11-11 13:42:46 +0900

    collection

--- Chapter12/collection.arraylist/MemberArrayList.java
+++ Chapter12/collection.arraylist/MemberArrayList.java
@@ -22,7 +22,7 @@ public class MemberArrayList {
 				return true;
 			}
 		}
-		System.out.println(memberId + "°¡ Á¸ÀçÇÏÁö ¾Ê½À´Ï´Ù.");
+		System.out.println(memberId + "ê°€ ì¡´ì¬í•˜ì§€ ì•ŠìŠµë‹ˆë‹¤.");
 		return false;
 	}
 	
